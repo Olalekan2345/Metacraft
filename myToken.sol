ac0eca474cb5da2cfe0fe70be8a30e524e4ce57e
Olalekan2345 <126165695+Olalekan2345@users.noreply.github.com>
2023-05-29 05:26:50 +0100

    Update myToken.sol

--- myToken.sol
+++ myToken.sol
@@ -1,41 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-/*
-       REQUIREMENTS
-    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
-    2. Your contract will have a mapping of addresses to balances (address => uint)
-    3. You will have a mint function that takes two parameters: an address and a value. 
-       The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
-    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
-       and from the balance of the “sender”.
-    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
-       to the amount that is supposed to be burned.
+/**
+
+@title MyToken
+
+@dev A simple ERC20 token contract representing a fictional football club named "Manchester United" (MUN).
+
+The contract allows minting and burning tokens and keeps track of token balances for each address.
 */
-
 contract MyToken {
+// Public variables
+string public tokenName = "MANCHESTER UNITED"; // The name of the token
+string public tokenAbbrv = "MUN"; // An abbreviation or symbol for the token
+uint public totalSupply = 0; // Total supply of tokens
 
-    // public variables here
-    string public tokenName = "TUNMISE";
-    string public tokenAbbrv = "TUM";
-    uint public totalSupply = 0;
+// Mapping variable to store balances
+mapping(address => uint) public balances; // Mapping from address to token balance
 
-    // mapping variable here
-    mapping(address => uint) public balances;
+/**
 
-    // mint function
-    function mint (address _address, uint _value) public{
-        totalSupply += _value;
-        balances[_address] += _value;
-    }
+@dev Mint function
+@param _address The address to mint tokens for
+@param _value The amount of tokens to mint
+*/
+function mint(address _address, uint _value) public {
+totalSupply += _value; // Increase the total supply
+balances[_address] += _value; // Increase the balance of the specified address
+}
+/**
 
-    // burn function
-     function burn (address _address, uint _value) public{
-         if (balances[_address] >= _value){
-        totalSupply -= _value;
-        balances[_address] -= _value;
-    }
-     }
+@dev Burn function
+@param _address The address to burn tokens from
+@param _value The amount of tokens to burn
+*/
+function burn(address _address, uint _value) public {
+if (balances[_address] >= _value) { // Check if the address has enough tokens to burn
+totalSupply -= _value; // Decrease the total supply
+balances[_address] -= _value; // Decrease the balance of the specified address
+}
+}
 }
